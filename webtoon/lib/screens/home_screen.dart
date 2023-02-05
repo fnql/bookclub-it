e8a0abc6487e62b6a688c7eb6b186daec97cb494
woogi <dbwhddnr00@naver.com>
2023-02-05 17:38:14 +0900

    웹툰 장르, 줄거리 추가

--- webtoon/lib/screens/home_screen.dart
+++ webtoon/lib/screens/home_screen.dart
@@ -1,34 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:webtoon/models/webtoon_model.dart';
 import 'package:webtoon/services/api_service.dart';
+import 'package:webtoon/widgets/webtoon_widget.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -43,6 +24,43 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           textAlign: TextAlign.center,
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                Expanded(child: makeList(snapshot))
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: ((context, index) {
+        var webtoon = snapshot.data![index];
+        return Webtoon(
+          id: webtoon.id,
+          thumb: webtoon.thumb,
+          title: webtoon.title,
+        );
+      }),
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
---
+++ webtoon/lib/widgets/webtoon_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:webtoon/screens/derail_screen.dart';
+
+class Webtoon extends StatelessWidget {
+  final String id, thumb, title;
+  const Webtoon({
+    super.key,
+    required this.id,
+    required this.thumb,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: ((context) => DetailScreen(
+                  id: id,
+                  thumb: thumb,
+                  title: title,
+                )),
+          ),
+        );
+      },
+      child: Column(
+        children: [
+          Hero(
+            tag: id,
+            child: Container(
+                width: 250,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 7,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                  ],
+                ),
+                child: Image.network(thumb)),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
